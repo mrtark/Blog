81b7446c25099b6a40de4edeb3d184ba47a57189
mrtark <murat_ark97@hotmail.com>
2023-01-28 13:06:48 +0300

    apiExeptions

---
+++ src/main/java/com/mrtark/exception/ResourceAuthorizedException.java
@@ -0,0 +1,11 @@
+package com.mrtark.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class ResourceAuthorizedException extends RuntimeException{
+    public ResourceAuthorizedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mrtark/exception/ResourceCreatedException.java
@@ -0,0 +1,11 @@
+package com.mrtark.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CREATED)
+public class ResourceCreatedException extends RuntimeException{
+    public ResourceCreatedException(String message) {
+        super(message);
+    }
+}
