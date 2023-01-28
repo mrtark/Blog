eb1a1286b4a51f0634c5bb2fe7f8b4a124f89a15
mrtark <murat_ark97@hotmail.com>
2023-01-28 14:13:35 +0300

    RegisterEntity

--- src/main/java/com/mrtark/data/entity/RegisterEntity.java
+++ src/main/java/com/mrtark/data/entity/RegisterEntity.java
@@ -1,4 +1,30 @@
 package com.mrtark.data.entity;
 
-public class RegisterEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "register_1")
+public class RegisterEntity extends BaseEntity implements Serializable {
+    public static final Long serialVersionUID=1L;
+    @Column(name = "username",columnDefinition = "varchar(255) default 'username not input!!'")
+    private String username;
+    private String surname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "telephone")
+    private String telephone;
+    @Column(name = "password")
+    private String password;
+
 }
