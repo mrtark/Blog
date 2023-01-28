ba07ccc929212bc8998c561ed15efc282ae9c2a7
mrtark <murat_ark97@hotmail.com>
2023-01-28 14:30:43 +0300

    auditing++

---
+++ src/main/java/com/mrtark/auditing/AuditorAwareBean.java
@@ -0,0 +1,12 @@
+package com.mrtark.auditing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+@Configuration
+public class AuditorAwareBean {
+    @Bean
+    public AuditorAware<String> auditorAwareMethod(){
+        return new AuditorAwareImpl();
+    }
+}
