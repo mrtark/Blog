d2dd5f0c72d1a283a1c618461bbc0eb39da4cae1
mrtark <murat_ark97@hotmail.com>
2023-01-28 12:51:30 +0300

    PasswordEncoderBean

--- src/main/java/com/mrtark/bean/PasswordEncoderBean.java
+++ src/main/java/com/mrtark/bean/PasswordEncoderBean.java
@@ -3,10 +3,12 @@ package com.mrtark.bean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 public class PasswordEncoderBean {
     @Bean
-    public BCryptPasswordEncoder passwordEncoderMethod(){
+    public PasswordEncoder passwordEncoderMethod(){
         return new BCryptPasswordEncoder();
     }
 }
