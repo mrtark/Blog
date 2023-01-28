07364c5d4a8cbd5583a209c1b0abacafa069b8bb
mrtark <murat_ark97@hotmail.com>
2023-01-28 14:32:05 +0300

    auditing+++

--- src/main/java/com/mrtark/BlogApplication.java
+++ src/main/java/com/mrtark/BlogApplication.java
@@ -2,9 +2,11 @@ package com.mrtark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication()
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
 public class BlogApplication {
 
     public static void main(String[] args) {
