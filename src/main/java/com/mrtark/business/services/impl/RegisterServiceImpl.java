3cdec630b0a091663814102e297c4308d417e41b
mrtark <murat_ark97@hotmail.com>
2023-01-29 14:44:51 +0300

    EntityToDto / DtoToEntity / create

--- src/main/java/com/mrtark/business/services/impl/RegisterServiceImpl.java
+++ src/main/java/com/mrtark/business/services/impl/RegisterServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mrtark.business.services.impl;
 
+import com.mrtark.bean.ModelMapperBean;
+import com.mrtark.bean.PasswordEncoderBean;
 import com.mrtark.business.dto.RegisterDTO;
 import com.mrtark.business.services.IRegisterService;
 import com.mrtark.data.entity.RegisterEntity;
+import com.mrtark.data.repository.IRegisterRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -14,19 +17,27 @@ import java.util.Map;
 @Log4j2
 @Service
 public class RegisterServiceImpl implements IRegisterService {
+    private final IRegisterRepository IRegisterRepository;
+    private final ModelMapperBean modelMapperBean;
+    private final PasswordEncoderBean passwordEncoderBean;
     @Override
     public RegisterDTO EntityToDto(RegisterEntity registerEntity) {
+        modelMapperBean.modelMapperMethod().map(registerEntity,RegisterDTO.class);
         return null;
     }
 
     @Override
     public RegisterEntity DtoToEntity(RegisterDTO registerDTO) {
+        modelMapperBean.modelMapperMethod().map(registerDTO,RegisterEntity.class);
         return null;
     }
 
     @Override
     public RegisterDTO registerCreate(RegisterDTO registerDTO) {
-        return null;
+        registerDTO.setPassword(passwordEncoderBean.passwordEncoderMethod().encode(registerDTO.getPassword()));
+        RegisterEntity registerEntity = IRegisterRepository.save(DtoToEntity(registerDTO));
+        registerDTO.setId(registerEntity.getId());
+        return registerDTO;
     }
 
     @Override
