b6cb76a61426b8a1687dfb713bbce5eb57873d0c
mrtark <murat_ark97@hotmail.com>
2023-02-01 09:39:01 +0300

    IRegisterAPI

--- src/main/java/com/mrtark/controller/api/IRegisterAPI.java
+++ src/main/java/com/mrtark/controller/api/IRegisterAPI.java
@@ -3,13 +3,18 @@ package com.mrtark.controller.api;
 import com.mrtark.business.dto.RegisterDTO;
 import org.springframework.http.ResponseEntity;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
 public interface IRegisterAPI {
-    ResponseEntity<?> createApi(RegisterDTO registerDTO);
-    ResponseEntity<List<RegisterDTO>> listApi();
-    ResponseEntity<RegisterDTO> findApi(Long id, RegisterDTO registerDTO);
-    ResponseEntity<?> updateApi(Long id, RegisterDTO registerDTO);
-    ResponseEntity<Map<String,String>> deleteApi (Long id);
+    String getProfileRegister(String name);
+    void getHeaderInformationRegister(Map<String,String> headers);
+    ResponseEntity<?> getAppInformationRegister(HttpServletRequest request, HttpServletResponse response);
+    ResponseEntity<?> createApiRegister(RegisterDTO registerDTO);
+    ResponseEntity<List<RegisterDTO>> listApiRegister();
+    ResponseEntity<RegisterDTO> findApiRegister(Long id, RegisterDTO registerDTO);
+    ResponseEntity<?> updateApiRegister(Long id, RegisterDTO registerDTO);
+    ResponseEntity<Map<String,String>> deleteApiRegister (Long id);
 }
