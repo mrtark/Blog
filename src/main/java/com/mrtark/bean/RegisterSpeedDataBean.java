0ce9632b94c7ee9899924cdccbce11e719f6c130
mrtark <murat_ark97@hotmail.com>
2023-02-08 13:47:42 +0300

    rv

--- src/main/java/com/mrtark/bean/RegisterSpeedDataBean.java
+++ src/main/java/com/mrtark/bean/RegisterSpeedDataBean.java
@@ -16,28 +16,28 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Configuration
 public class RegisterSpeedDataBean {
-    private final ModelMapperBean modelMapperBean;
-    private final IRegisterRepository iRegisterRepository;
-    @Value("${data.surname}")
-    private String  MessagesProperties;
-    private final PasswordEncoderBean passwordEncoderBean;
-    private final ServletContext servletContext;
-    @Bean
-    CommandLineRunner createXRegister(){
-        String applicationProperties = servletContext.getInitParameter("my_special.name");
-        return (args)->{
-            for (int i = 0; i <=3; i++) {
-                RegisterDTO registerDTO = RegisterDTO
-                        .builder()
-                        .username(applicationProperties)
-                        .surname(MessagesProperties)
-                        .email(UUID.randomUUID().toString().concat("@mrtark.com"))
-                        .password(passwordEncoderBean.passwordEncoderMethod().encode("123mrtark"))
-                        .telephone("123 456 7890")
-                        .build();
-                RegisterEntity registerEntity =  modelMapperBean.modelMapperMethod().map(registerDTO,RegisterEntity.class);
-                iRegisterRepository.save(registerEntity);
-            }
-        };
-    }
+//    private final ModelMapperBean modelMapperBean;
+//    private final IRegisterRepository iRegisterRepository;
+//    @Value("${data.surname}")
+//    private String  MessagesProperties;
+//    private final PasswordEncoderBean passwordEncoderBean;
+//    private final ServletContext servletContext;
+//    @Bean
+//    CommandLineRunner createXRegister(){
+//        String applicationProperties = servletContext.getInitParameter("my_special.name");
+//        return (args)->{
+//            for (int i = 0; i <=3; i++) {
+//                RegisterDTO registerDTO = RegisterDTO
+//                        .builder()
+//                        .username(applicationProperties)
+//                        .surname(MessagesProperties)
+//                        .email(UUID.randomUUID().toString().concat("@mrtark.com"))
+//                        .password(passwordEncoderBean.passwordEncoderMethod().encode("123mrtark"))
+//                        .telephone("123 456 7890")
+//                        .build();
+//                RegisterEntity registerEntity =  modelMapperBean.modelMapperMethod().map(registerDTO,RegisterEntity.class);
+//                iRegisterRepository.save(registerEntity);
+//            }
+//        };
+//    }
 }
